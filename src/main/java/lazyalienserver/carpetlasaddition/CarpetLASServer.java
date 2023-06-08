882272a64b30feb19dce1b07d4fef08b9c23be66
Fanzhitianyu <fanzhiyuxian@163.com>
2023-06-08 19:53:53 +0800

    Initial commit

---
+++ src/main/java/lazyalienserver/carpetlasaddition/CarpetLASServer.java
@@ -0,0 +1,18 @@
+package lazyalienserver.carpetlasaddition;
+
+import carpet.CarpetExtension;
+import net.fabricmc.api.ModInitializer;
+
+public class CarpetLASServer implements CarpetExtension, ModInitializer {
+
+    public String modName(){
+        return "Carpet-LAS-Addition";
+    }
+    public String modID(){
+        return "carpetlasaddition";
+    }
+    @Override
+    public void onInitialize() {
+
+    }
+}
