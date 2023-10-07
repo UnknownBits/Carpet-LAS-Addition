784203dbcf9b0faf7af694b5df737c12c5a25ce7
Fanzhitianyu <fanzhiyuxian@163.com>
2023-10-07 23:20:53 +0800

    1.2.1 alpha-fix

--- src/main/java/lazyalienserver/carpetlasaddition/records/RecordList.java
+++ src/main/java/lazyalienserver/carpetlasaddition/records/RecordList.java
@@ -9,21 +9,24 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class RecordList {
-    public static final Map<String,Record> recordList=new ConcurrentHashMap<>();
-    public static final Record commandRecord= RecordList.RegisterRecord(new CommandRecord());
-    public static final Record commandBlockRecord=RecordList.RegisterRecord(new CommandBlockRecord());
-    public static Record RegisterRecord(Record record){
-        recordList.put(record.identifier,record);
+    public static final Map<String, Record> recordList = new ConcurrentHashMap<>();
+    public static final Record commandRecord = RecordList.RegisterRecord(new CommandRecord());
+    public static final Record commandBlockRecord = RecordList.RegisterRecord(new CommandBlockRecord());
+
+    public static Record RegisterRecord(Record record) {
+        recordList.put(record.identifier, record);
         return record;
     }
-    public static void saveAllRecord(){
-        if (!recordList.isEmpty()){
-            for (Map.Entry<String,Record> entry:recordList.entrySet()){
-                FileUtils.writeFile(entry.getValue().getRecordsDir().resolve(DateTimeUtils.getNowDate()+".log"),entry.getValue().getRecord());
+
+    public static void saveAllRecord() {
+        for (Map.Entry<String, Record> entry : recordList.entrySet()) {
+            if (!entry.getValue().getRecord().isEmpty()) {
+                FileUtils.writeFile(entry.getValue().getRecordsDir().resolve(DateTimeUtils.getNowDate() + ".log"), entry.getValue().getRecord());
             }
         }
     }
-    public static void load(){
+
+    public static void load() {
         DateTimeUtils.addDayScheduleEvent(RecordList::saveAllRecord);
     }
 }
